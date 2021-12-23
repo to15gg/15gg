a1f6eff6524df3e2417531bcbecec92addf021b4
BaekJaeIn <baek2506@gmail.com>
2021-12-23 16:52:37 +0900

    tailwindconfig colors 초기설정 (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "teal-400": "#B1DED3",
+      "teal-500": "#47C2A4",
+      "teal-600": "#35AA8D",
+      rose: "#E36E6E",
+      blue: "6E78D2",
+      "almost-white": "#FDFDFD",
+      "almost-black": "#222222",
+      "gray-100": "#F8F8F8",
+      "gray-200": "#F0F0F0",
+      "gray-300": "#C2C2C2",
+      "gray-400": "#5C5C5C",
+    },
+    fontFamily: {
+      sans: ["Noto Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
