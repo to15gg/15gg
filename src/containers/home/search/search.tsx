7e3e793d7ed59bb6e27c830b6593f446bf0ff2a0
soilSpoon <lee111dae11@gmail.com>
2021-12-24 17:51:33 +0900

    위치 조정

--- src/containers/home/search/search.tsx
+++ src/containers/home/search/search.tsx
@@ -3,7 +3,7 @@ import Form from "./form";
 
 export default function Search() {
   return (
-    <div className="absolute self-end w-full max-w-[550px] h-[50%]">
+    <div className="absolute self-end w-full max-w-[550px] h-[calc(50%_+_4.5rem)]">
       <div className="flex flex-col px-[2rem] rounded-[5px] bg-[white]">
         <Form />
         <Autocomplete />
