ad3d47ae8d646c5c6ec14a35510b581b2dfa4d0e
baekjaein <baek2506@gmail.com>
2021-12-24 23:05:23 +0900

    Tailwind.config 초안 작성

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,31 @@ module.exports = {
       "teal-400": "#B1DED3",
       "teal-500": "#47C2A4",
       "teal-600": "#35AA8D",
-      rose: "#E36E6E",
-      blue: "6E78D2",
-      "almost-white": "#FDFDFD",
-      "almost-black": "#222222",
+      "rose-500": "#E36E6E",
+      "blue-500": "6E78D2",
+      // Light mode
+      "almostblack": "#222222",
+      "background": "#FDFDFD",
       "gray-100": "#F8F8F8",
-      "gray-200": "#F0F0F0",
-      "gray-300": "#C2C2C2",
-      "gray-400": "#5C5C5C",
+      "gray-300": "#F0F0F0",
+      "gray-500": "#C2C2C2",
+      "gray-700": "#5C5C5C",
+      "gray-900": "#383838",
+      // Dark mode
+      "background-dark": "#222222",
+      "almostwhite": "#FDFDFD",
+      "gray-100-dark": "#383838",
+      "gray-300-dark": "#5C5C5C",
+      "gray-500-dark": "#C2C2C2",
+      "gray-700-dark": "#F0F0F0",
+      "gray-900-dark": "#F8F8F8",
+      // Dim
+      "dim": "#000000",
+    },
+    opacity: {
+      // Dim Opacity (Use with color "dim")
+      "dim-light": ".75",
+      "dim-dark": ".50",
     },
     fontFamily: {
       sans: ["Noto Sans", "sans-serif"],
