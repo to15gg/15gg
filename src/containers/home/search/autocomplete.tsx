de67a6375aacfc884c65247fed251a5c425b37fc
soilSpoon <lee111dae11@gmail.com>
2021-12-26 18:20:47 +0900

    디자인 수정 사항 반영

--- src/containers/home/search/autocomplete.tsx
+++ src/containers/home/search/autocomplete.tsx
@@ -17,38 +17,44 @@ export default function Autocomplete() {
   }
 
   return (
-    <ul className="border-t-[1px] border-t-[#D9D9D9] space-y-[1.5rem] py-[2.5rem]">
-      {items.map((item, index) => {
-        const { level, lp, name, profileIconUrl, tierRank } = item;
-        const backgroundClasses =
-          index === selectedIndex ? "bg-teal-400" : "bg-transparent";
+    <div className="relative border-t-[1px] border-t-[#D9D9D9] ">
+      <p className="absolute top-[1rem] right-0 text-[10px]">
+        화살표 키로 검색 결과를 이동할 수 있습니다.
+      </p>
+      <ul className="space-y-[1.5rem] py-[2.5rem]">
+        {items.map((item, index) => {
+          const { level, lp, name, profileIconUrl, tierRank } = item;
+          const backgroundClasses =
+            index === selectedIndex ? "bg-teal-400" : "bg-transparent";
 
-        const levelText = `level ${level}`;
-        const tierText = tierRank === null ? "Unranked" : `${tierRank} ${lp}LP`;
+          const levelText = `level ${level}`;
+          const tierText =
+            tierRank === null ? "Unranked" : `${tierRank} ${lp}LP`;
 
-        return (
-          <li key={name}>
-            <Link href={`/search/${name}`}>
-              <a
-                onMouseEnter={() => setSelectedIndex(index)}
-                className={`flex items-center space-x-[2rem] py-[1.5rem] ${backgroundClasses}`}
-              >
-                <div className="relative w-[5rem] h-[5rem] rounded-full overflow-hidden">
-                  <Image
-                    src={`${location.protocol}${profileIconUrl}`}
-                    alt="프로필 이미지"
-                    layout="fill"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <p>{name}</p>
-                  <p>{[levelText, tierText].join(" / ")}</p>
-                </div>
-              </a>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={name}>
+              <Link href={`/search/${name}`}>
+                <a
+                  onMouseEnter={() => setSelectedIndex(index)}
+                  className={`flex items-center space-x-[2rem] py-[1.5rem] ${backgroundClasses}`}
+                >
+                  <div className="relative w-[5rem] h-[5rem] rounded-full overflow-hidden">
+                    <Image
+                      src={`${location.protocol}${profileIconUrl}`}
+                      alt="프로필 이미지"
+                      layout="fill"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <p>{name}</p>
+                    <p>{[levelText, tierText].join(" / ")}</p>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
--- src/containers/home/search/search.tsx
+++ src/containers/home/search/search.tsx
@@ -3,8 +3,8 @@ import Form from "./form";
 
 export default function Search() {
   return (
-    <div className="absolute self-end w-full max-w-[550px] h-[calc(50%_+_4.5rem)]">
-      <div className="flex flex-col px-[2rem] rounded-[5px] bg-[white]">
+    <div className="absolute self-end width w-full max-w-[84.75rem] h-[calc(50%_+_4.5rem)]">
+      <div className="flex flex-col px-[2rem] rounded-[5px] bg-[white] min-w-min">
         <Form />
         <Autocomplete />
       </div>
