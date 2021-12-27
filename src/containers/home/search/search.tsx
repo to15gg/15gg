d0cf05754f59632dc36a0bf8969641fbbabc2e3d
SeungWoo Lee <smoothlee7104@gmail.com>
2021-12-27 14:37:23 +0900

    Modify: 검색창 box-shadow 스타일 추가 (#10)

--- src/containers/home/search/search.tsx
+++ src/containers/home/search/search.tsx
@@ -3,8 +3,8 @@ import Form from "./form";
 
 export default function Search() {
   return (
-    <div className="absolute self-end width w-full max-w-[84.75rem] h-[calc(50%_+_4.5rem)]">
-      <div className="flex flex-col px-[2rem] rounded-[5px] bg-[white] min-w-min">
+    <div className="absolute self-end width w-full max-w-[84.75rem] h-[calc(50%_+_4.5rem)] z-10">
+      <div className="flex flex-col px-[2rem] rounded-[5px] bg-[white] min-w-min shadow-lg">
         <Form />
         <Autocomplete />
       </div>
